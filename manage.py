e1b2b64c68185199876bd3a0e488a8be0d3c020e
Kenstogram <admin@waitingblock.com>
2018-06-11 18:25:00 -0500

    updated manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
     try:
         from django.core.management import execute_from_command_line
-<<<<<<< HEAD
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
@@ -20,12 +19,11 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
-=======
+        
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
->>>>>>> 37c99181c9a6b95433d60f8c8ef9af5731096435
     execute_from_command_line(sys.argv)
